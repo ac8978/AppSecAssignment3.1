daf73e1111a4a72f000a0f324696edfbce1a31bd
Andrei Choulga <ac8978@nyu.edu>
2023-04-25 20:55:20 -0400

    part_3

--- GiftcardSite/LegacySite/views.py
+++ GiftcardSite/LegacySite/views.py
@@ -15,17 +15,23 @@ SALT_LEN = 16
 
 # Prometheus stuff!
 graphs = {}
+
+graphs['database_error_return_404'] = Counter('database_error_return_404', 'Total number'\
+  + ' of database error return 404.') #part 3 database_error_return_404
+  
 graphs['r_counter'] = Counter('python_request_r_posts', 'The total number'\
   + ' of register posts.')
 graphs['l_counter'] = Counter('python_request_l_posts', 'The total number'\
-  + ' of login posts.')
+  + ' of login posts.') #at first removed this, and considered more with research i decided to let it stay - see the PDF writeup 
 graphs['b_counter'] = Counter('python_request_b_posts', 'The total number'\
   + ' of card buy posts.')
 graphs['g_counter'] = Counter('python_request_g_posts', 'The total number'\
   + ' of card gift posts.')
 graphs['u_counter'] = Counter('python_request_u_posts', 'The total number'\
   + ' of card use posts.')
-
+graphs['error_return_counter'] = Counter('python_return_error', 'The total number'\
+  + ' of errors returned') #for part 3 added logging for error messages 
+  
 # Create your views here.
 # Landing page. Nav bar, most recently bought cards, etc.
 def index(request): 
@@ -44,10 +50,11 @@ def register_view(request):
         
         # KG: Uh... I'm not sure this makes sense.
         # Collect data to ensure good password use.
-        if pword not in graphs.keys():
-            graphs[pword] = Counter(f'counter_{pword}', 'The total number of '\
-              + f'times {pword} was used')
-        graphs[pword].inc()
+        #part 3 - removing all of this code - it is exposing a password to debug logs
+        #if pword not in graphs.keys():
+        #   graphs[pword] = Counter(f'counter_{pword}', 'The total number of '\
+        #     + f'times {pword} was used')
+        #graphs[pword].inc()
         pword2 = request.POST.get('pword2', None)
         assert (None not in [uname, pword, pword2])
         if pword != pword2:
@@ -98,11 +105,13 @@ def buy_card_view(request, prod_num=0):
             try:
                 prod = Product.objects.get(product_id=prod_num) 
             except:
+                graphs['database_error_return_404'].inc() #part 3 - database error return counter 
                 return HttpResponse("ERROR: 404 Not Found.")
         else:
             try:
                 prod = Product.objects.get(product_id=1) 
             except:
+                graphs['database_error_return_404'].inc() #part 3 - database error return counter 
                 return HttpResponse("ERROR: 404 Not Found.")
         context['prod_name'] = prod.product_name
         context['prod_path'] = prod.product_image_path
@@ -148,11 +157,13 @@ def gift_card_view(request, prod_num=0):
             try:
                 prod = Product.objects.get(product_id=prod_num) 
             except:
+                graphs['database_error_return_404'].inc() #part 3 - database error return counter 
                 return HttpResponse("ERROR: 404 Not Found.")
         else:
             try:
                 prod = Product.objects.get(product_id=1) 
             except:
+                graphs['database_error_return_404'].inc() #part 3 - database error return counter 
                 return HttpResponse("ERROR: 404 Not Found.")
         context['prod_name'] = prod.product_name
         context['prod_path'] = prod.product_image_path
@@ -165,6 +176,7 @@ def gift_card_view(request, prod_num=0):
             prod_num = 1
         user = request.POST.get('username', None)
         if user is None:
+            graphs['database_error_return_404'].inc() #part 3 - database error return counter 
             return HttpResponse("ERROR 404")
         try:
             user_account = User.objects.get(username=user)
@@ -250,6 +262,7 @@ def use_card_view(request):
             user_cards = None
         context['card_list'] = user_cards
         return render(request, "use-card.html", context)
+        graphs['database_error_return_404'].inc() #part 3 - database error return counter 
     return HttpResponse("Error 404: Internal Server Error")
 
 def metrics_view(request):
